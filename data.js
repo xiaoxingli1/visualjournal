1a325e1d1c8b47b600ea7c5ca7ee2b51de8f580f
x.li5 <x.li5@tue.nl>
2026-02-06 01:00:49 +0100

    style

---
+++ data.js
@@ -0,0 +1,24 @@
+// Edit this file to add new years / subcategories / images.
+window.GALLERY = {
+  nameOnCover: "Your Real Name",
+
+  handDrawn: {
+    "2025": [
+      { src: "img/hand-drawn/2025/001.jpg", title: "" },
+      { src: "img/hand-drawn/2025/002.jpg", title: "" }
+    ],
+    "Originals": [
+      { src: "img/hand-drawn/originals/001.jpg", title: "" }
+    ]
+  },
+
+  digital: {
+    "2025": [
+      { src: "img/digital/2025/001.jpg", title: "" }
+    ],
+    "Originals": [
+      { src: "img/digital/originals/001.jpg", title: "" }
+    ]
+  }
+};
+
